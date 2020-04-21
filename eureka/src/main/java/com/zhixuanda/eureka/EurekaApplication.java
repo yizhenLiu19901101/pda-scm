bbc6df713fbf54015e5f01956c7031ea496d8ec5
milo19901101 <liuyizhen19901101@163.com>
2020-04-21 11:15:00 +0800

    配置文件修改

--- eureka/src/main/java/com/zhixuanda/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/zhixuanda/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.zhixuanda.eureka;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
